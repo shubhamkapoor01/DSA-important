e9e2092c9bf93fd0f5291ffb5b46839396035ba3
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2021-12-28 23:27:45 +0400

    Update Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        bool done = false;
+        int n = nums.size();
         
-        for (int i = nums.size() - 1; i > 0; i --) {
-            if (nums[i] > nums[i - 1]) {
-                
+        for (int i = n - 1; i > 0; i --) {
+            if (nums[i - 1] < nums[i]) {
                 int j = i;
-                while (j < nums.size() && nums[j] > nums[i - 1]) {
+                while ( j < n && nums[j] > nums[i - 1]) {
                     j ++;
                 }
                 j --;
-                
-                int t = nums[i - 1];
-                nums[i - 1] = nums[j];
-                nums[j] = t;
-                
+                int temp = nums[j];
+                nums[j] = nums[i - 1];
+                nums[i - 1] = temp;
                 reverse(nums.begin() + i, nums.end());
-                done = true;
-                break;
+                return;
             }
         }
         
-        if (!done) {
-            reverse(nums.begin(), nums.end());
-        }
+        reverse(nums.begin(), nums.end());
         return;
     }
 };
