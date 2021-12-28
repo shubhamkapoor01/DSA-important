e5afdacd642515639840f1b7228d182925103fb5
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2021-12-28 23:57:05 +0400

    Create Sort Colors.cpp

---
+++ Sort Colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+        
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                mid ++;
+                low ++;
+                
+            } else if (nums[mid] == 1) {
+                mid ++;
+                
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high --;   
+            }
+        }
+    }
+};
