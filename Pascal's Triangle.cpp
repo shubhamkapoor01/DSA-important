2cd993bcbe851c7007d3cc7fff0c23efe3b0870b
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2021-12-28 23:12:45 +0400

    Create Pascal's Triangle.cpp

---
+++ Pascal's Triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (numRows <= 0) {
+            return {};
+        }
+        
+        if (numRows == 1) {
+            return {{1}};
+        }
+        
+        if (numRows == 2) {
+            return {{1}, {1,1}};
+        }
+        
+        vector<vector<int>> ans = {{1}, {1,1}};
+        for (int i = 2; i < numRows; i ++) {
+            vector<int> temp = {1};
+            
+            for (int j = 0; j < ans[i - 1].size() - 1; j ++) {
+                temp.push_back(ans[i - 1][j] + ans[i - 1][j + 1]);
+            }
+            temp.push_back(1);
+            ans.push_back(temp);
+        }
+        
+        return ans;
+    }
+};
