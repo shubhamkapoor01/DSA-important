4c077665598030f38eccddae61b45eb656c4bac3
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2021-12-30 09:56:28 +0400

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int x = 0;
-        int y = matrix[0].size() - 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int rowl = 0;
+        int rowr = n - 1;
         
-        while (x >= 0 && y >= 0 && x < matrix.size() && y < matrix[0].size()) {
-            if (matrix[x][y] == target) {
-                return true;
-            } else if (matrix[x][y] > target) {
-                y --;
+        while (rowl <= rowr) {
+            int rowm = (rowl + rowr) / 2;
+            if (target <= matrix[rowm][m - 1] && target >= matrix[rowm][0]) {
+                int l = 0;
+                int r = m - 1;
+                
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+                    
+                    if (matrix[rowm][mid] == target) {
+                        return true;
+                    } else if (target > matrix[rowm][mid]) {
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                return false;
+                
+            } else if (target > matrix[rowm][m - 1]) {
+                rowl = rowm + 1;
             } else {
-                x ++;
+                rowr = rowm - 1;
             }
         }
         
