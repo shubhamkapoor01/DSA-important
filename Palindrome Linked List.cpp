3478c0c27f49e4580da290cd12eaf1fa1566731d
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-01-02 13:52:07 +0400

    Update Palindrome Linked List.cpp

--- Palindrome Linked List.cpp
+++ Palindrome Linked List.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        ListNode* fast = head;
+    ListNode* getMiddle(ListNode* head) {
         ListNode* slow = head;
+        ListNode* fast = head;
         
-        while (fast && fast -> next) {
+        while(fast and fast -> next and fast -> next -> next) {
             fast = fast -> next -> next;
             slow = slow -> next;
         }
         
-        ListNode* t = head;
-        while (slow && t) {
-            if (slow -> val == t -> val) {
-                return false;
-            }
-            
-            slow = slow -> next;
-            t = t -> next;
+        return slow;
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+        ListNode* curr = head;
+        
+        while(curr) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
         }
         
+        return prev;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        
+        ListNode* mid = getMiddle(head);
+        mid -> next = reverseList(mid -> next);
+        mid = mid -> next;
+        
+        while(mid) {
+            if(head -> val != mid -> val)
+                return false;
+            mid = mid -> next;
+            head = head -> next;
+        }
         return true;
     }
 };
