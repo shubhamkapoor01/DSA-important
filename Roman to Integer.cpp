e882fd8291104dda47a1460419c40212213da157
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-01-11 12:28:07 +0400

    Create Roman to Integer.cpp

---
+++ Roman to Integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int result = 0;
+        
+        for (int i = s.size() - 1; i >= 0; i --) {
+            if (i - 1 >= 0 && m[s[i - 1]] < m[s[i]]) {
+                result = result + m[s[i]] - m[s[i - 1]];
+                i --;
+            } else {
+                result = result + m[s[i]];
+            }
+        }
+        
+        return result;
+    }
+};
