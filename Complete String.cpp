dde574fa0e42e213964af3b2eab1199aa8a9a6f1
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-01-15 14:19:42 +0400

    Create Complete String.cpp

---
+++ Complete String.cpp
@@ -0,0 +1,82 @@
+struct trieNode {
+    char val;
+    bool ending;
+    vector<trieNode*> next {vector<trieNode*> (26, nullptr)};
+};
+
+class Trie {
+    trieNode* root;
+    
+public:
+    Trie() {
+        this -> root = new trieNode;
+        this -> root -> val = ' ';
+        this -> root -> ending = false;
+    }
+    
+    void insert(string word) {
+        int n = word.size();
+        trieNode* temp = root;
+        
+        for (int i = 0; i < n; i ++) {
+            int curr = word[i] - 'a';
+            
+            if (temp -> next[curr]) {
+                temp = temp -> next[curr];
+                
+            } else {
+                trieNode* child = new trieNode;
+                child -> val = word[i];
+                child -> ending = false;
+                
+                temp -> next[curr] = child;
+                temp = child;
+            }
+            
+            if (i == n - 1) {
+                temp -> ending = true;
+            }
+        }
+    }
+    
+    bool search(string word) {
+        int n = word.size();
+        trieNode* temp = root;
+        
+        for (int i = 0; i < n; i ++) {
+            int curr = word[i] - 'a';
+            
+            if (temp -> next[curr] && temp -> next[curr] -> ending) {
+                temp = temp -> next[curr];
+            } else {
+                return false;
+            }
+            
+            if (i == n - 1 && temp -> ending) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+string completeString(int n, vector<string> &a){
+    Trie root;
+    
+    for (int i = 0; i < n; i ++) {
+        root.insert(a[i]);
+    }
+    
+    string ans = "";
+    for (int i = 0; i < n; i ++) {
+        if ((a[i].size() > ans.size() && root.search(a[i])) || 
+            (a[i].size() == ans.size() && a[i] < ans && root.search(a[i]))) {
+            ans = a[i];
+        }
+    }
+    
+    if (ans == "") {
+        return "None";
+    }
+    return ans;
+}
