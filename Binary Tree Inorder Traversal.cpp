946aaa9b097989f1d96797e307e0974a03bd572b
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-01-16 12:04:42 +0400

    Create Binary Tree Inorder Traversal.cpp

---
+++ Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        if (!root) {
+            return inorder;
+        }
+        
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        
+        while (true) {
+            if (node) {
+                st.push(node);
+                node = node -> left;
+                
+            } else {
+                if (st.empty()) {
+                    break;
+                }
+                
+                node = st.top();
+                st.pop();
+                inorder.push_back(node -> val);
+                node = node -> right;
+            }
+        }
+        
+        return inorder;
+    }
+};
