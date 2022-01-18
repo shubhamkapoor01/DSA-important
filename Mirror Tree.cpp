d2a203b8817797e9aa1279d3c5dfd950e34a99f3
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-01-18 12:38:11 +0400

    Create Mirror Tree.cpp

---
+++ Mirror Tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+  
+    void mirror (Node* node) {
+        if (!node) {
+            return;
+        }
+        
+        Node* temp = node -> left;
+        node -> left = node -> right;
+        node -> right = temp;
+        
+        mirror(node -> left);
+        mirror(node -> right);
+        return;
+    }
+};
