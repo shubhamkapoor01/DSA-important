d109d6697975d6d019e30382aa7e9eac2616e93c
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-01-20 13:46:34 +0400

    Update Construct Binary Search Tree from Preorder Traversal.cpp

--- Construct Binary Search Tree from Preorder Traversal.cpp
+++ Construct Binary Search Tree from Preorder Traversal.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    TreeNode* solve(int is, int ie, int ps, int pe, vector<int> &inorder, 
-                    vector<int> &preorder, map<int, int> &mp) {
-        
-        if (is > ie || ps > pe) {
+    TreeNode* solve(vector<int> &preorder, int &i, int ub) {
+        if (i == preorder.size() || preorder[i] > ub) {
             return nullptr;
         }
         
-        TreeNode* root = new TreeNode(preorder[ps]);
-        int inRoot = mp[root -> val];
-        int lsize = inRoot - is;
-        
-        root -> left = solve(is, inRoot - 1, ps + 1, ps + lsize, inorder, preorder, mp);
-        root -> right = solve(inRoot + 1, ie, ps + lsize + 1, pe, inorder, preorder, mp);
+        TreeNode* root = new TreeNode(preorder[i ++]);
+        root -> left = solve(preorder, i, root -> val);
+        root -> right = solve(preorder, i, ub);
         return root;
     }
     
     TreeNode* bstFromPreorder(vector<int> &preorder) {
-        vector<int> inorder = preorder;
-        sort(inorder.begin(), inorder.end());
-        
-        map<int, int> mp;
-        for (int i = 0; i < inorder.size(); i ++) {
-            mp[inorder[i]] = i;
-        }
-        
-        return solve(0, inorder.size() - 1, 0, preorder.size() - 1, inorder, preorder, mp);
+        int i = 0;
+        return solve(preorder, i, INT_MAX);
     }
 };
