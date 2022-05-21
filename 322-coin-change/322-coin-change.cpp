e7b1cf5737f39d17d75a86ec8f6f60e8abf04630
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-21 11:26:10 +0530

    Time: 196 ms (17.63%), Space: 14.4 MB (68.59%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,36 +1,23 @@
 class Solution {
-private:
-    int n;
-    
-    int solve(vector<int>& coins, int idx, int amount, vector<vector<int>>& dp) {
-        if (amount == 0) {
-            return 0;
-        }
-        
-        if (idx == n) {
-            return 1e9;
-        }
-        
-        if (dp[idx][amount] != -1) {
-            return dp[idx][amount];
-        }
-        
-        int ans = 1e9;
-        ans = min(ans, solve(coins, idx + 1, amount, dp));
-        
-        if (amount >= coins[idx]) {
-            ans = min(ans, 1 + solve(coins, idx, amount - coins[idx], dp));
-        }
-        
-        dp[idx][amount] = ans;
-        return ans;
-    }
-    
 public:
     int coinChange(vector<int>& coins, int amount) {
-        n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int> (amount + 1, -1));
-        int ans = solve(coins, 0, amount, dp);
-        return ans != 1e9 ? ans : -1;
+        int n = coins.size();
+        vector<int> dp(amount + 1, 1e9);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= amount; j ++) {
+                if (j - coins[i - 1] >= 0) {
+                    dp[j] = min(dp[j], 1 + dp[j - coins[i - 1]]);
+                }
+            }
+        }
+        
+        // for (int i = 0; i <= amount; i ++) {
+        //     cout << dp[i] << " ";
+        // }
+        // cout << endl;
+        
+        return dp[amount] >= 1e9 ? -1 : dp[amount];
     }
 };
