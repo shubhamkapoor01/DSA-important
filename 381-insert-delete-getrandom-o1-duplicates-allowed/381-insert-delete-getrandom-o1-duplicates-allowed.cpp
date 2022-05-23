808f9205814c687cbfbd348d0867cf34ae1da856
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-23 18:01:58 +0530

    Time: 311 ms (57.56%), Space: 107.7 MB (43.29%) - LeetHub

--- 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,7 +1,7 @@
 class RandomizedCollection {
 private:
     vector<int> v;
-    unordered_map<int, vector<int>> mp;
+    unordered_map<int, unordered_set<int>> mp;
     
     
 public:
@@ -11,7 +11,7 @@ public:
     bool insert(int val) {
         bool present = mp.find(val) != mp.end();
         v.push_back(val);
-        mp[val].push_back(v.size() - 1);
+        mp[val].insert(v.size() - 1);
         return !present;
     }
     
@@ -21,17 +21,24 @@ public:
         }
         
         int last = v.back();
-        v.pop_back();
-        v[mp[val].back()] = last;
+        int del = *mp[val].begin();
         
-        for (auto &i: mp[last]) {
-            if (i == v.size()) {
-                i = mp[val].back();
-                break;
+        if (last == val) {
+            v.pop_back();
+            mp[last].erase(v.size());
+            if (mp[last].size() == 0) {
+                mp.erase(last);
             }
+            return true;
         }
         
-        mp[val].pop_back();
+        v.pop_back();
+        v[del] = last;
+        
+        mp[last].insert(del);
+        mp[last].erase(v.size());
+        
+        mp[val].erase(del);
         if (mp[val].size() == 0) {
             mp.erase(val);
         }
