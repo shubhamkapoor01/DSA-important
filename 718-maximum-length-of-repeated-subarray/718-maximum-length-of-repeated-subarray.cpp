4ff25b63112e2ae247257a52e7bb205f12891c10
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-24 18:13:19 +0530

    Time: 663 ms (10.19%), Space: 11.8 MB (93.97%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -3,16 +3,25 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
-        int longestCommonSubstring = 0;
+        
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+        
+        int LCsubstring = 0;
+        
         for (int i = 1; i <= n; i ++) {
             for (int j = 1; j <= m; j ++) {
                 if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j - 1]);
-                    longestCommonSubstring = max(longestCommonSubstring, dp[i][j]);
+                    curr[j] = max(curr[j], 1 + prev[j - 1]);
+                    LCsubstring = max(LCsubstring, curr[j]);
                 }
             }
+            for (int j = 0; j <= m; j ++) {
+                prev[j] = curr[j];
+                curr[j] = 0;
+            }
         }
-        return longestCommonSubstring;
+        
+        return LCsubstring;
     }
 };
