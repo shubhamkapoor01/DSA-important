e693f9dac5c83e71328fcc2cbb01a440b3569373
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-24 13:21:31 +0530

    Time: 2 ms (58.64%), Space: 6.4 MB (24.98%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,32 +1,19 @@
 class Solution {
-private:
-    int solve(string& s, int idx, vector<int>& dp) {
-        if (idx == s.size()) {
-            return 1;
-        }
-        
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-        
-        int ans = 0;
-        
-        if ((s[idx] - '0') != 0) {
-            ans += solve(s, idx + 1, dp);
-        }
-        
-        if (idx + 1 < s.size() && (s[idx] - '0') != 0 && (s[idx] - '0') * 10 + (s[idx + 1] - '0') <= 26) {
-            ans += solve(s, idx + 2, dp);
-        }
-        
-        dp[idx] = ans;
-        return ans;
-    }
-    
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return solve(s, 0, dp);
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        
+        for (int i = 1; i <= n; i ++) {
+            if ((s[i - 1] - '0') != 0) {
+                dp[i] += dp[i - 1];
+            }
+            if (i >= 2 && (s[i - 2] - '0') != 0 && (s[i - 2] - '0') * 10 + (s[i - 1] - '0') <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[n];
     }
 };
