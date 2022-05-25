8c0e8469a05d35246d097a669cbd7d8f2c45c97e
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-25 20:17:06 +0530

    Time: 38 ms (24.28%), Space: 6.5 MB (92.94%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,26 +4,31 @@ public:
         int n = word1.size();
         int m = word2.size();
         
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
         
-        for (int i = 0; i <= n; i ++) {
-            dp[i][0] = i;
-        }
-        
+        curr[0] = 1;
         for (int j = 0; j <= m; j ++) {
-            dp[0][j] = j;
+            prev[j] = j;
         }
         
         for (int i = 1; i <= n; i ++) {
             for (int j = 1; j <= m; j ++) {
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
+                    
                 } else {
-                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+                    curr[j] = 1 + min({prev[j], curr[j - 1], prev[j - 1]});
                 }
             }
+            
+            for (int j = 0; j <= m; j ++) {
+                prev[j] = curr[j];
+                curr[j] = 0;
+            }
+            curr[0] = i + 1;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
