838625bff5a5fcb4d3f4dbacb0671f649acbe1e3
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-29 10:41:59 +0530

    Time: 60 ms (70.18%), Space: 15.7 MB (79.69%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -4,28 +4,21 @@ public:
         int n = word.size();
         int len = 0;
         
-        vector<vector<int>> freq(n, vector<int> (26, 0));
+        vector<int> bitset(n);
         
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < word[i].size(); j ++) {
-                freq[i][word[i][j] - 'a'] ++;
+                bitset[i] |= 1 << (word[i][j] - 'a');
             }
         }
         
         for (int i = 0; i < n - 1; i ++) {
             for (int j = i + 1; j < n; j ++) {
-                bool possible = true;
-                
-                for (int k = 0; k < 26; k ++) {
-                    if (freq[i][k] > 0 && freq[j][k] > 0) {
-                        possible = false;
-                        break;
-                    }
+                if (bitset[i] & bitset[j]) {
+                    continue;
                 }
                 
-                if (possible) {
-                    len = max(len, (int)word[i].size() * (int)word[j].size());
-                }
+                len = max(len, (int)word[i].size() * (int)word[j].size());
             }
         }
         
