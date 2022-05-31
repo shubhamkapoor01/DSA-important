d2f9577086c235f924820861aeff26ff53bf26f6
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-05-31 17:51:07 +0530

    Time: 62 ms (23.77%), Space: 31.9 MB (86.11%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int n = customers.size();
+        int currSatisfied = 0;
+        int maxSatisfied = 0;
+        int windowStart = 0;
+        int windowEnd = 0;
+        int startIdx = 0;
+        int endIdx = 0;
+        
+        while (windowEnd < n) {
+            if (grumpy[windowEnd]) {
+                currSatisfied += customers[windowEnd];
+            }
+            
+            if (windowEnd - windowStart + 1 > minutes) {
+                if (grumpy[windowStart]) {
+                    currSatisfied -= customers[windowStart];
+                }
+                windowStart ++;
+            }
+            
+            if (currSatisfied > maxSatisfied) {
+                maxSatisfied = currSatisfied;
+                startIdx = windowStart;
+                endIdx = windowEnd;
+            }
+            
+            windowEnd ++;
+        }
+        
+        int totalSatisfied = 0;
+        
+        for (int i = 0; i < n; i ++) {
+            if (i >= startIdx && i <= endIdx) {
+                totalSatisfied += customers[i];
+                
+            } else {
+                totalSatisfied += grumpy[i] ? 0 : customers[i];
+            }
+        }
+        
+        return totalSatisfied;
+    }
+};
