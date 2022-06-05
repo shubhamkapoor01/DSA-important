7c8f305c705110a93b8ba51bc85e6100b423e5df
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-05 10:10:52 +0530

    Time: 750 ms (11.11%), Space: 109.8 MB (33.33%) - LeetHub

---
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -0,0 +1,66 @@
+class TextEditor {
+    deque<char> before;
+    deque<char> after;
+    
+    void fillString(deque<char>& before, string& s) {
+        int maxLen = 10;
+        while (before.size() && maxLen --) {
+            s.push_back(before.back());
+            before.pop_back();
+        }
+        reverse(s.begin(), s.end());
+        for (auto &ch: s) {
+            before.push_back(ch);
+        }
+    }
+    
+public:
+    TextEditor() {
+    }
+    
+    void addText(string text) {
+        for (auto &ch: text) {
+            before.push_back(ch);
+        }
+    }
+    
+    int deleteText(int k) {
+        int deleted = 0;
+        while(before.size() && k --) {
+            before.pop_back();
+            deleted ++;
+        }
+        return deleted;
+    }
+    
+    string cursorLeft(int k) {
+        while (before.size() && k --) {
+            char ch = before.back();
+            after.push_front(ch);
+            before.pop_back();
+        }
+        string s;
+        fillString(before, s);
+        return s;
+    }
+    
+    string cursorRight(int k) {
+        while (after.size() && k --) {
+            char ch = after.front();
+            before.push_back(ch);
+            after.pop_front();
+        }
+        string s;
+        fillString(before, s);
+        return s;
+    }
+};
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * TextEditor* obj = new TextEditor();
+ * obj->addText(text);
+ * int param_2 = obj->deleteText(k);
+ * string param_3 = obj->cursorLeft(k);
+ * string param_4 = obj->cursorRight(k);
+ */
