d7db927d0534513f879128cac34b108416756bba
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-05 10:32:11 +0530

    Time: 517 ms (11.11%), Space: 112.8 MB (33.33%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.cpp
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -1,27 +1,13 @@
 class TextEditor {
-    deque<char> before;
-    deque<char> after;
-    
-    void fillString(deque<char>& before, string& s) {
-        int maxLen = 10;
-        while (before.size() && maxLen --) {
-            s.push_back(before.back());
-            before.pop_back();
-        }
-        reverse(s.begin(), s.end());
-        for (auto &ch: s) {
-            before.push_back(ch);
-        }
-    }
+    string before;
+    string after;
     
 public:
     TextEditor() {
     }
     
     void addText(string text) {
-        for (auto &ch: text) {
-            before.push_back(ch);
-        }
+        before += text;
     }
     
     int deleteText(int k) {
@@ -36,23 +22,21 @@ public:
     string cursorLeft(int k) {
         while (before.size() && k --) {
             char ch = before.back();
-            after.push_front(ch);
+            after.push_back(ch);
             before.pop_back();
         }
-        string s;
-        fillString(before, s);
-        return s;
+        int len = min((int)before.size(), 10);
+        return before.substr(before.size() - len);
     }
     
     string cursorRight(int k) {
         while (after.size() && k --) {
-            char ch = after.front();
+            char ch = after.back();
             before.push_back(ch);
-            after.pop_front();
+            after.pop_back();
         }
-        string s;
-        fillString(before, s);
-        return s;
+        int len = min((int)before.size(), 10);
+        return before.substr(before.size() - len);
     }
 };
 
