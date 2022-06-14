321197df2e0173a167511736a927aa7392331385
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-14 19:21:56 +0530

    Time: 629 ms (6.85%), Space: 110.8 MB (41.97%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -4,34 +4,32 @@ public:
         int n = points.size();
         int m = points[0].size();
         
-        vector<long long> prev(m, 0);
-        vector<long long> curr(m, 0);
+        vector<long long> dp(m, 0);
         
         for (int i = 0; i < m; i ++) {
-            prev[i] = points[0][i];
+            dp[i] = points[0][i];
         }
         
         for (int i = 1; i < n; i ++) {
             vector<long long> pref(m, 0);
             vector<long long> suff(m, 0);
             
-            pref[0] = prev[0];
-            suff[m - 1] = prev[m - 1];
+            pref[0] = dp[0];
+            suff[m - 1] = dp[m - 1];
             
             for (int j = 1; j < m; j ++) {
-                pref[j] = max(pref[j - 1] - 1LL, prev[j]);
+                pref[j] = max(pref[j - 1] - 1LL, dp[j]);
             }
             
             for (int j = m - 2; j >= 0; j --) {
-                suff[j] = max(suff[j + 1] - 1LL, prev[j]);
+                suff[j] = max(suff[j + 1] - 1LL, dp[j]);
             }
             
             for (int j = 0; j < m; j ++) {
-                curr[j] = points[i][j] + max(pref[j], suff[j]);
-                prev[j] = curr[j];
+                dp[j] = points[i][j] + max(pref[j], suff[j]);
             }
         }
         
-        return *max_element(prev.begin(), prev.end());
+        return *max_element(dp.begin(), dp.end());
     }
 };
