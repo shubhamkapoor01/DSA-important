4355b0fe5655f98b29b7bde8a2aeb0599c291a07
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-22 12:09:57 +0530

    Time: 224 ms (5.02%), Space: 10 MB (61.91%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -2,31 +2,31 @@ class Solution {
 private:
     int partition(vector<int>& nums, int left, int right) {
         int n = nums.size();
-        int pivot = nums[right];
         int i = left;
         int j = left;
-        while (i < right) {
-            if (nums[i] > pivot) {
-                i ++;
+        while (j < right) {
+            if (nums[j] > nums[right]) {
+                j ++;
             } else {
                 swap(nums[i], nums[j]);
                 i ++;
                 j ++;
             }
         }
-        swap(nums[j], nums[i]);
-        return j;
+        swap(nums[i], nums[j]);
+        return i;
     }
     
     int kthMax(vector<int>& nums, int l, int r, int k) {
         int idx = partition(nums, l, r);
-        if (idx < k) return kthMax(nums, idx + 1, r, k);
         if (idx > k) return kthMax(nums, l, idx - 1, k);
-        else return nums[k];
+        if (idx < k) return kthMax(nums, idx + 1, r, k);
+        return nums[idx];
     }
     
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        return kthMax(nums, 0, nums.size() - 1, nums.size() - k);
+        int n = nums.size();
+        return kthMax(nums, 0, n - 1, n - k);
     }
 };
