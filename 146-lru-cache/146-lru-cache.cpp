148463c6dcaf61a47cbaa3e6d8c3275d4dd20a42
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-27 15:41:36 +0530

    Time: 797 ms (11.53%), Space: 165.1 MB (71.67%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,9 +1,8 @@
-class DoublyListNode {
-public:
+struct DoublyListNode {
     int key;
     int val;
-    DoublyListNode* next;
     DoublyListNode* prev;
+    DoublyListNode* next;
     
     DoublyListNode(int key, int val) {
         this -> key = key;
@@ -13,38 +12,63 @@ public:
     }
 };
 
-class LRUCache {
+class DoublyLinkedList {
 private:
-    unordered_map<int, DoublyListNode*> mp;
     DoublyListNode* head;
     DoublyListNode* tail;
-    int currSize;
-    int maxSize;
-    
-    void moveToBack(DoublyListNode* node) {
-        if (!node) return;
-        
-        DoublyListNode* before = node -> prev;
-        DoublyListNode* after = node -> next;
-        
-        if (before) before -> next = after;
-        if (after) after -> prev = before;
-        
-        node -> prev = tail -> prev;
-        tail -> prev -> next = node;
-        node -> next = tail;
-        tail -> prev = node;
-        return;
-    }
-    
-    
     
 public:
-    LRUCache(int capacity) {
+    DoublyLinkedList() {
         this -> head = new DoublyListNode(-1, -1);
         this -> tail = new DoublyListNode(-1, -1);
         this -> head -> next = this -> tail;
         this -> tail -> prev = this -> head;
+    }
+    
+    DoublyListNode* getFront() {
+        return head -> next;
+    }
+    
+    DoublyListNode* getBack() {
+        return tail -> prev;
+    }
+    
+    void pushBack(DoublyListNode* node) {
+        node -> prev = tail -> prev;
+        tail -> prev -> next = node;
+        node -> next = tail;
+        tail -> prev = node;
+    }
+    
+    void popFront() {
+        DoublyListNode* temp = head -> next;
+        head -> next = temp -> next;
+        temp -> next -> prev = head;
+        delete temp;
+    }
+    
+    void removeNode(DoublyListNode* node) {
+        DoublyListNode* before = node -> prev;
+        DoublyListNode* after = node -> next;
+        if (before) before -> next = after;
+        if (after) after -> prev = before;
+    }
+    
+    void moveBack(DoublyListNode* node) {
+        removeNode(node);
+        pushBack(node);
+    }
+};
+
+class LRUCache {
+private:
+    unordered_map<int, DoublyListNode*> mp;
+    DoublyLinkedList list;
+    int currSize;
+    int maxSize;
+    
+public:
+    LRUCache(int capacity) {
         this -> maxSize = capacity;
         this -> currSize = 0;
     }
@@ -54,7 +78,7 @@ public:
             return -1;
         }
         
-        moveToBack(mp[key]);
+        list.moveBack(mp[key]);
         return mp[key] -> val;
     }
     
@@ -62,21 +86,18 @@ public:
         if (mp.find(key) != mp.end()) {
             mp[key] -> val = value;
             mp[key] -> key = key;
-            moveToBack(mp[key]);
+            list.moveBack(mp[key]);
             return;
         }
         
         if (currSize < maxSize) {
             mp[key] = new DoublyListNode(key, value);
-            moveToBack(mp[key]);
+            list.pushBack(mp[key]);
             currSize ++;
             
         } else {
-            DoublyListNode* temp = head -> next;
-            mp.erase(temp -> key);
-            head -> next = temp -> next;
-            temp -> next -> prev = head;
-            delete temp;
+            mp.erase(list.getFront() -> key);
+            list.popFront();
             currSize --;
             put(key, value);
         }
