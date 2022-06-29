0323d8515da0d0775aa96cf9097ec34cafb081a4
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-29 23:40:44 +0530

    Time: 19 ms (51.75%), Space: 8.2 MB (77.57%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,30 +1,40 @@
 class Solution {
-private:
-    bool solve(string& s, int i, vector<int>& dp, vector<string>& words) {
-        if (i == s.size()) {
-            return true;
+public:
+//     bool wordBreak(string s, vector<string>& words) {
+//         int n = s.size();
+//         vector<bool> dp(n + 1, false);
+//         dp[0] = true;
+        
+//         for (int i = 1; i <= n; i ++) {
+//             for (auto &word: words) {
+//                 if (i >= word.size() && word == s.substr(i - word.size(), word.size())) {
+//                     dp[i] = dp[i] || dp[i - word.size()];
+//                 }
+//             }
+//         }
+        
+//         return dp[n];
+//     }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> set;
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        for(auto it : wordDict){
+            set.insert(it);
         }
-        
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        
-        bool ans = false;
-        
-        for (auto &word: words) {
-            if (word == s.substr(i, word.size())) {
-                ans |= solve(s, i + word.size(), dp, words);
+        dp[n] = true;
+        for(int i = n-1; i>=0; i--){
+            string inter;
+            for(int j = i;j<n;j++){
+               inter+=s[j];
+               if(set.find(inter)!=set.end()){
+                   if(dp[j+1]) dp[i] = true;
+               }
             }
         }
-        
-        dp[i] = ans;
-        return ans;
-    }
-    
-public:
-    bool wordBreak(string s, vector<string>& words) {
-        int n = s.size();
-        vector<int> dp(n, -1);
-        return solve(s, 0, dp, words);
+       return dp[0]; 
     }
 };
+
+
