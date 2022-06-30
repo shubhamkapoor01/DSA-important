dd9457a924e409222c6e5b1450abfa22f7746834
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-30 11:41:23 +0530

    Time: 1366 ms (5.90%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,42 @@
 class Solution {
+private:
+    int partition(vector<int>& nums, int l, int r) {
+        int i = l;
+        int j = l;
+        
+        while (i < r) {
+            if (nums[i] > nums[r]) {
+                i ++;
+                
+            } else {
+                swap(nums[i], nums[j]);
+                i ++;
+                j ++;
+            }
+        }
+        
+        swap(nums[j], nums[i]);
+        return j;
+    }
+        
+    int getMedian(vector<int>& nums, int l, int r) {
+        int pos = partition(nums, l, r);
+        if (pos > nums.size() / 2) return getMedian(nums, l, pos - 1);
+        if (pos < nums.size() / 2) return getMedian(nums, pos + 1, r);
+        return nums[pos];
+    }
+    
 public:
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
         int cnt = 0;
+        
+        int median = getMedian(nums, 0, n - 1);
+        
         for (int i = 0; i < n; i ++) {
-            cnt += abs(nums[i] - nums[n / 2]);
+            cnt += abs(nums[i] - median);
         }
+        
         return cnt;
     }
 };
