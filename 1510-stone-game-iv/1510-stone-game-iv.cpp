c3402e44ac33980bc4c932314fc6b72db0386218
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-03 15:26:36 +0530

    Time: 464 ms (17.56%), Space: 7.9 MB (51.71%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,32 +1,16 @@
 class Solution {
-private:
-    int alice, bob;
-    
 public:
-    Solution() {
-        this -> alice = 1;
-        this -> bob = 0;
-    }
-    
     bool winnerSquareGame(int n) {
-        vector<vector<int>> dp(n + 1, vector<int> (2, 0));
-        dp[0][alice] = bob;
-        dp[0][bob] = alice;
-        dp[1][alice] = alice;
-        dp[1][bob] = bob;
+        vector<int> dp(n + 1, false);
+        dp[0] = false;
+        dp[1] = true;
         
         for (int i = 2; i <= n; i ++) {
             for (int j = 1; j * j <= i; j ++) {
-                dp[i][alice] |= dp[i - j * j][bob];
-                dp[i][bob] |= !dp[i - j * j][alice];
-                
-                if (dp[i][alice] && dp[i][bob]) {
-                    break;
-                }
+                dp[i] |= !dp[i - j * j];
             }
-            dp[i][bob] = !dp[i][bob];
         }
         
-        return dp[n][alice];
+        return dp[n];
     }
 };
