3a59f51c2d51b20a226325c47a1c456d4aa63673
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-04 12:53:20 +0530

    Time: 45 ms (43.91%), Space: 12.8 MB (31.28%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,15 +1,22 @@
 class Solution {
 private:
+    int dp[21][40000];
+    
     int solve(vector<int>& nums, int i, int t) {
         if (i == nums.size()) {
             return !t;
         }
         
-        return solve(nums, i + 1, t + nums[i]) + solve(nums, i + 1, t - nums[i]);
+        if (dp[i][t + 20000] != -1){ 
+            return dp[i][t + 20000];
+        }
+        
+        return dp[i][t + 20000] = solve(nums, i + 1, t + nums[i]) + solve(nums, i + 1, t - nums[i]);
     }
     
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp, -1, sizeof(dp));
         return solve(nums, 0, target);
     }
 };
