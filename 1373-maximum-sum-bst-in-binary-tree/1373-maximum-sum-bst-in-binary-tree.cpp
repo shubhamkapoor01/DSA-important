3ce34ce5fbed217cc30fbdb748febbb3e5ddd131
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-06 19:30:06 +0530

    Time: 725 ms (7.19%), Space: 186.2 MB (22.36%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -13,28 +13,35 @@
 class Solution {
 private:
     vector<int> solve(TreeNode* root, int& mx) {
-        if (!root) return {0, INT_MAX, INT_MIN};
+        if (!root) return {0, INT_MAX, INT_MIN, 1};
         
         if (!root -> left && !root -> right) {
-            mx = max(mx, root -> val);
-            return {root -> val, root -> val, root -> val};
+            return {root -> val, root -> val, root -> val, 1};
         }
         
         vector<int> left = solve(root -> left, mx);
         vector<int> right = solve(root -> right, mx);
         
-        if (!left.size() || !right.size() || left[2] >= root -> val || right[1] <= root -> val) {
-            return {};
+        if (left[3]) {
+            mx = max(mx, left[0]);
         }
         
-        mx = max(mx, root -> val + left[0] + right[0]);
-        return {root -> val + left[0] + right[0], min(left[1], root -> val), max(right[2], root -> val)};
+        if (right[3]) {
+            mx = max(mx, right[0]);
+        }
+        
+        if (left[2] < root -> val && right[1] > root -> val && left[3] && right[3]) {
+            mx = max(mx, root -> val + left[0] + right[0]);
+            return {root -> val + left[0] + right[0], min(root -> val, left[1]), max(root -> val, right[2]), 1};
+        }
+        
+        return {root -> val, root -> val, root -> val, 0};
     }
     
 public:
     int maxSumBST(TreeNode* root) {
         int mx = 0;
-        vector<int> t = solve(root, mx);
+        vector<int> v = solve(root, mx);
         return mx;
     }
 };
