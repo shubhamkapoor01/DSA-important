2a0103c16257073a17985c1e1f33234ed53685f1
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-07 14:17:02 +0530

    Time: 9 ms (47.23%), Space: 6.4 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-//     bool solve(string& s1, string& s2, string& s3, int i, int j) {
-//         if (i == s1.size() && j == s2.size()) return true;
-        
-//         if (dp[i][j] != -1) return dp[i][j];
-        
-//         if (s1[i] == s3[i + j] && s2[j] == s3[i + j]) {
-//             return dp[i][j] = solve(s1, s2, s3, i + 1, j) || solve(s1, s2, s3, i, j + 1);
-            
-//         } else if (s1[i] == s3[i + j]) {
-//             return dp[i][j] = solve(s1, s2, s3, i + 1, j);
-        
-//         } else if (s2[j] == s3[i + j]) {
-//             return dp[i][j] = solve(s1, s2, s3, i, j + 1);
-            
-//         } else {
-//             return dp[i][j] = false;
-//         }
-//     }
-    
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size();
         int m = s2.size();
@@ -26,28 +7,28 @@ public:
         
         if (k != n + m) return false;
         
-        vector<int> dp(m + 1, false);
+        vector<int> curr(m + 1, false);
         
         for (int i = 0; i <= n; i ++) {
             for (int j = 0; j <= m; j ++) {
                 if (i == 0 && j == 0) {
-                    dp[j] = true;
+                    curr[j] = 1;
                     
-                } else if (i > 0 && j > 0) {
-                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                } else if (i > 0 && j > 0 && s1[i - 1] == s3[i + j - 1] && s2[j - 1] == s3[i + j - 1]) {
+                    curr[j] = curr[j] | curr[j - 1];
                     
-                } else if (i > 0) {
-                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]);
+                } else if (i > 0 && s1[i - 1] == s3[i + j - 1]) {
+                    curr[j] = curr[j];
                 
-                } else if (j > 0) {
-                    dp[j] = (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                } else if (j > 0 && s2[j - 1] == s3[i + j - 1]) {
+                    curr[j] = curr[j - 1];   
                 
                 } else {
-                    dp[j] = false;
+                    curr[j] = false;
                 }
             }
         }
         
-        return dp[m];
+        return curr[m];
     }
 };
