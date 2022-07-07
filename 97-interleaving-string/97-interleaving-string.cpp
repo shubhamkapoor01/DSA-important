755ee9e73f9060e6fd28abfe6e565c9345db8e05
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-07 14:16:15 +0530

    Time: 6 ms (61.58%), Space: 6.2 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,35 +1,53 @@
 class Solution {
-private:
-    int dp[101][201];
-    
 public:
-    Solution() {
-        memset(dp, -1, sizeof(dp));
-    }
-    
-    bool solve(string& s1, string& s2, string& s3, int i, int j, int k) {
-        if (i == s1.size() && j == s2.size()) return k == s3.size();
-        if (k == s3.size()) return i == s1.size() && j == s2.size();
-        if (i == s1.size()) return s2.substr(j) == s3.substr(k);
-        if (j == s2.size()) return s1.substr(i) == s3.substr(k);
+//     bool solve(string& s1, string& s2, string& s3, int i, int j) {
+//         if (i == s1.size() && j == s2.size()) return true;
         
-        if (dp[i][k] != -1) return dp[i][k];
+//         if (dp[i][j] != -1) return dp[i][j];
         
-        if (s1[i] == s3[k] && s2[j] == s3[k]) {
-            return dp[i][k] = solve(s1, s2, s3, i + 1, j, k + 1) || solve(s1, s2, s3, i, j + 1, k + 1);
+//         if (s1[i] == s3[i + j] && s2[j] == s3[i + j]) {
+//             return dp[i][j] = solve(s1, s2, s3, i + 1, j) || solve(s1, s2, s3, i, j + 1);
             
-        } else if (s1[i] == s3[k]) {
-            return dp[i][k] = solve(s1, s2, s3, i + 1, j, k + 1);
+//         } else if (s1[i] == s3[i + j]) {
+//             return dp[i][j] = solve(s1, s2, s3, i + 1, j);
         
-        } else if (s2[j] == s3[k]) {
-            return dp[i][k] = solve(s1, s2, s3, i, j + 1, k + 1);
+//         } else if (s2[j] == s3[i + j]) {
+//             return dp[i][j] = solve(s1, s2, s3, i, j + 1);
             
-        } else {
-            return dp[i][k] = false;
-        }
-    }
+//         } else {
+//             return dp[i][j] = false;
+//         }
+//     }
     
     bool isInterleave(string s1, string s2, string s3) {
-        return solve(s1, s2, s3, 0, 0, 0);
+        int n = s1.size();
+        int m = s2.size();
+        int k = s3.size();
+        
+        if (k != n + m) return false;
+        
+        vector<int> dp(m + 1, false);
+        
+        for (int i = 0; i <= n; i ++) {
+            for (int j = 0; j <= m; j ++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                    
+                } else if (i > 0 && j > 0) {
+                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                    
+                } else if (i > 0) {
+                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]);
+                
+                } else if (j > 0) {
+                    dp[j] = (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                
+                } else {
+                    dp[j] = false;
+                }
+            }
+        }
+        
+        return dp[m];
     }
 };
