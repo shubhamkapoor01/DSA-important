7b2894a6319a16b8621dd91906ddd2ffc20cfb06
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-09 01:45:42 +0530

    Time: 13 ms (33.87%), Space: 10.5 MB (16.90%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
-private:
-    int solve(vector<int>& stones, int i, int s, int sum, vector<vector<int>>& dp) {
-        if (i == stones.size()) {
-            return abs(sum - 2 * s);
-        }
+// private:
+//     int solve(vector<int>& stones, int i, int s, int sum, vector<vector<int>>& dp) {
+//         if (i == stones.size()) {
+//             return abs(sum - 2 * s);
+//         }
         
-        if (dp[i][s] != -1) {
-            return dp[i][s];
-        }
+//         if (dp[i][s] != -1) {
+//             return dp[i][s];
+//         }
         
-        int ans = solve(stones, i + 1, s, sum, dp);
-        ans = min(ans, solve(stones, i + 1, s + stones[i], sum, dp));
+//         int ans = solve(stones, i + 1, s, sum, dp);
+//         ans = min(ans, solve(stones, i + 1, s + stones[i], sum, dp));
         
-        dp[i][s] = ans;
-        return ans;
-    }
+//         dp[i][s] = ans;
+//         return ans;
+//     }
     
 public:
     int lastStoneWeightII(vector<int>& stones) {
@@ -24,7 +24,18 @@ public:
             sum += i;
         }
         
-        vector<vector<int>> dp(n, vector<int> (sum + 1, -1));
-        return solve(stones, 0, 0, sum, dp);
+        vector<vector<int>> dp(n + 1, vector<int> (sum + 1, 0));
+        
+        for (int i = 0; i <= n; i ++) {
+            for (int s = 0; s <= sum; s ++) {
+                if (i == 0) {
+                    dp[i][s] = abs(sum - 2 * s);
+                } else {
+                    dp[i][s] = min(dp[i - 1][s], ((s - stones[i - 1] >= 0) ? dp[i - 1][s - stones[i - 1]] : 1000000));
+                }
+            }
+        }
+        
+        return dp[n][sum];
     }
 };
