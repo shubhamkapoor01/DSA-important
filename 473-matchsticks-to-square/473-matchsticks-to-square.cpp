e9decf0dadefeb21f5ac6a5d0d281e35584da257
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-07-12 13:06:32 +0530

    Time: 1084 ms (12.24%), Space: 294.8 MB (5.02%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    map<vector<int>, int> mp;   
+    map<pair<int, set<int>>, int> mp;   
     int side;
     
     bool solve(vector<int>& matchsticks, int i = 0, int a = 0, int b = 0, int c = 0, int d = 0) {
@@ -12,16 +12,16 @@ private:
             return a == b && a == c && a == d;
         }
         
-//         if (mp.find({i, a, b, c, d}) != mp.end()) {
-//             return mp[{i, a, b, c, d}];
-//         }
+        if (mp.find({i, {a, b, c, d}}) != mp.end()) {
+            return mp[{i, {a, b, c, d}}];
+        }
         
-        return solve(matchsticks, i + 1, a + matchsticks[i], b, c, d) || 
+        mp[{i, {a, b, c, d}}] = solve(matchsticks, i + 1, a + matchsticks[i], b, c, d) || 
                               solve(matchsticks, i + 1, a, b + matchsticks[i], c, d) || 
                               solve(matchsticks, i + 1, a, b, c + matchsticks[i], d) ||
                               solve(matchsticks, i + 1, a, b, c, d + matchsticks[i]);
         
-        // return mp[{i, a, b, c, d}];
+        return mp[{i, {a, b, c, d}}];
     }
     
 public:
