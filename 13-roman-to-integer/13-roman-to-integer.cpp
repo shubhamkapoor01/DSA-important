830fb830867479eabaddeea8a0452394081792b2
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-08-15 12:06:02 +0530

    Time: 34 ms (18.25%), Space: 8.1 MB (30.55%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> value;
         
-        value['I'] = 1;
-        value['V'] = 5;
-        value['X'] = 10;
-        value['L'] = 50;
-        value['C'] = 100;
-        value['D'] = 500;
-        value['M'] = 1000;
+        unordered_map<char, int> m;
         
-        int ans = 0;
-        int n = s.size();
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
         
-        if (n == 1) {
-            return value[s[0]];
-        }
+        int result = 0;
         
-        for (int i = 0; i < n - 1; i ++) {
-            if (value[s[i]] < value[s[i + 1]]) {
-                ans += value[s[i + 1]] - value[s[i]];
-                i ++;
-                
-                if (i == n - 2) {
-                    ans += value[s[i + 1]];
-                }
-                
+        for (int i = s.size() - 1; i >= 0; i --) {
+            if (i - 1 >= 0 && m[s[i - 1]] < m[s[i]]) {
+                result += m[s[i]] - m[s[i - 1]];
+                i --;
             } else {
-                ans += value[s[i]];
-                
-                if (i == n - 2) {
-                    ans += value[s[i + 1]];
-                }
+                result += m[s[i]];
             }
         }
         
-        return ans;
+        return result;
     }
 };
