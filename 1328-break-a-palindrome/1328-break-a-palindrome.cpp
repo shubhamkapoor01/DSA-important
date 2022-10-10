1cecf5ee88506c563f018d208e9fcfaa21fccdc9
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-10-10 21:52:53 +0530

    Time: 6 ms (16.14%), Space: 6.2 MB (78.51%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        int n = s.size();
+        if (n <= 1) return "";
+        for (int i = 0; i < n / 2; i ++) {
+            // cout << s[i] << " ";
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                return s;
+            }
+        }
+        // cout << endl;
+        s[n - 1] = 'b';
+        return s;
+    }
+};
