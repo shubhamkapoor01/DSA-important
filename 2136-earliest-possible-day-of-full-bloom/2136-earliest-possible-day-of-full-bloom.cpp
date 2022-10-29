70096a8a89076cbc19b2a7641f26326179201975
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-10-29 13:45:53 +0530

    Time: 702 ms (40.56%), Space: 158.6 MB (80.32%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
         int n = plantTime.size();
-        vector<pair<int, int>> v;
+        vector<pair<int, int>> plant(n);        
         
-        for (int i = 0; i < n; i ++) {
-            v.push_back({ growTime[i], plantTime[i] });
+        for(int i = 0; i < n; i ++) {
+            plant[i] = {growTime[i], plantTime[i]};
         }
         
-        sort(v.begin(), v.end(), greater<pair<int, int>>());
+        sort(plant.begin(), plant.end(),  greater<pair<int, int>>());
         
-        vector<pair<int, pair<int, int>>> t(n);
-        t[0] = { 0, { v[0].second, v[0].first + v[0].second }};
+        int day = 0;
         int ans = 0;
         
-        for (int i = 1; i < n; i ++) {
-            t[i].first = t[i - 1].second.first + 1;
-            t[i].second.first = t[i - 1].second.first + v[i].second;
-            t[i].second.second = t[i - 1].second.first + v[i].second + v[i].first;
-        }
-        
-        for (int i = 0; i < n; i ++) {
-            ans = max(ans, t[i].second.second);
+        for(int i = 0; i < n; i++) {
+            day += plant[i].second;
+            ans = max(ans, day + plant[i].first);
         }
         
         return ans;
