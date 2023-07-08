dada0ab7c001eb1539130eab24e955b0fcd96d2d
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2023-07-08 13:57:05 +0530

    Time: 463 ms (10.65%), Space: 65.3 MB (52.23%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -4,22 +4,21 @@ public:
     long long putMarbles(vector<int>& weights, int k) {
         this->n=weights.size();
         if(k==1 || n==2) return 0;
-
         priority_queue<int> maxHeap;
         priority_queue<int, vector<int>, greater<int>> minHeap;
         for(int i=1; i<n; i++){
             maxHeap.push(weights[i-1]+weights[i]);
             minHeap.push(weights[i-1]+weights[i]);
+            if(maxHeap.size()>=k) maxHeap.pop();
+            if(minHeap.size()>=k) minHeap.pop();
         }
-
-        long long maxi=weights[0]+weights[n-1], mini=maxi;
+        long long maxi=0, mini=0;
         for(int i=1; i<k; i++){
-            maxi+=maxHeap.top();
-            maxHeap.pop();
-            mini+=minHeap.top();
+            maxi+=minHeap.top();
             minHeap.pop();
+            mini+=maxHeap.top();
+            maxHeap.pop();
         }
-
         return maxi-mini;
     }
 };
